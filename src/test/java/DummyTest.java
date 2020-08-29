d819d395b7dd4eeae7e7505cde267323aa10e3fa
Luis Antonio <9704777+Antoreich@users.noreply.github.com>
2020-08-29 11:37:47 -0500

    Added pom.xml Added src/ files

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,20 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static junit.framework.Assert.assertTrue;
+
+public class DummyTest {
+    @Before
+    public void beforeTestDo(){
+        System.out.println("Antes de las pruebas");
+    }
+    @Test
+    public void dummyTest(){
+        System.out.println("This is the test");
+        assertTrue(true);
+    }
+    @After
+    public void afterTestDo(){
+        System.out.println("Despues de las pruebas");
+    }
+}
